0aed45ea9e590a9b8c2bca4743dec82df36bda19
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-13 05:27:44 +0000

    Variable

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,9 @@
+"use sctric" // treat all JS code as newer version
+
+console.log(3-3);
+console.log ("Logicmo Systems private Limited");
+// Symbol 
+
+console.log(typeof "Rahul Yadav");
+console.log(typeof null);
+console.log(typeof undefined);
