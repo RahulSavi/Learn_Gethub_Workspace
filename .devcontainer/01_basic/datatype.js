848f83978c7912411600f6ea92808879ebee3efd
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-13 11:18:13 +0000

    Object & function

---
+++ .devcontainer/01_basic/datatype.js
@@ -0,0 +1,28 @@
+// Primitive (Call by Vlaue)
+
+//7 type : string , Number,boolean,null,undefined, symbol,bigInt
+
+// Reference type datatypes (Non Primitive)
+
+// Arrays, objects,functions
+
+
+const id= Symbol('123456')
+const userid= Symbol('123456')
+
+console.log(id=== userid);
+
+const bigNumber = 1321321564897954545656n
+
+const cars=["Omini","swift","Auto"]
+let myobj={
+    name:"Rahul",
+    age :32,
+    location:"pune"
+}
+
+const myfunction=function(){
+console.log("Hello Rahul")
+}
+console.log(typeof bigNumber);
+
