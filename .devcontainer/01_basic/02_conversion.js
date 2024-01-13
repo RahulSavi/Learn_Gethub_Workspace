478b45858e87604238eac1df7476348709270ba3
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-13 10:31:29 +0000

    Learning conversion

---
+++ .devcontainer/01_basic/02_conversion.js
@@ -0,0 +1,19 @@
+let score ="33abc";
+
+console.log (typeof score)
+console.log (typeof (score))
+
+const valueinnumber= Number(score)
+
+console.log(typeof valueinnumber);
+console.log(valueinnumber);// provide the NaN 
+
+let isboolean="";
+let boolenislogedin=Boolean(isboolean)
+console.log(boolenislogedin);
+
+let number=33
+
+let convertstrinf =String(number);
+console.log(convertstrinf)
+console.log(typeof convertstrinf);
