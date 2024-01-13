2b1ebd026a6d7d197c440212aa0ef0b2b680dedb
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-13 10:50:46 +0000

    Learing More in conversion

--- .devcontainer/01_basic/02_conversion.js
+++ .devcontainer/01_basic/02_conversion.js
@@ -1,16 +1,16 @@
 let score ="33abc";
 
-console.log (typeof score)
-console.log (typeof (score))
+// console.log (typeof score)
+// console.log (typeof (score))
 
 const valueinnumber= Number(score)
 
-console.log(typeof valueinnumber);
-console.log(valueinnumber);// provide the NaN 
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);// provide the NaN 
 
 let isboolean="";
 let boolenislogedin=Boolean(isboolean)
-console.log(boolenislogedin);
+// console.log(boolenislogedin);
 
 let number=33
 
@@ -20,7 +20,7 @@ let convertstrinf =String(number);
 
 let value=3
 let negvalue =-value
-console.log(negvalue)
+// console.log(negvalue)
 
 let str1 ="hello"
 let str2 ="Logicmo Systems private Limited"
@@ -28,7 +28,16 @@ let str2 ="Logicmo Systems private Limited"
 let str3 =str1+str2;
 console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2")
-console.log("1"+2+2)
-console.log("1"-2)
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log("1"-2)
+
+console.log(true)
+
+let num1, num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter)
