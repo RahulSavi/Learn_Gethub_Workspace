1d055a684ebf40e64f916e951db72e83b7601651
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-15 12:11:53 +0000

    Learing String Method

---
+++ .devcontainer/01_basic/03_String.js
@@ -0,0 +1,13 @@
+// const name ="LogicMo Syetms Private Limited"
+// const department="Software support";
+
+// //console.log (`Company Name is : ${name}  deparmaent Name is : ${department}`)
+
+// const complayname = "       LogicMo System Private Limited         ";
+
+// console.log(complayname)
+// console.log(complayname.trimStart())
+
+const complaynamenew = "LogicMo Systems private limited"
+
+console.log(complaynamenew.split(' '))
