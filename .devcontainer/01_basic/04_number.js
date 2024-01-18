f039eb941a47057c3d27c9fb0e579218fb00aec1
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-18 10:05:02 +0000

    Learning Number data type

--- .devcontainer/01_basic/04_number.js
+++ .devcontainer/01_basic/04_number.js
@@ -1,17 +1,35 @@
-const score =400
-console.log(score)
+// const score =400
+// console.log(score)
 
-const balance =new Number(100)
+// const balance =new Number(100)
 
-console.log(balance)
+//console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(1))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
 
-const othernumer=235.23
+const othernumer = 235.23
 
 console.log(othernumer.toPrecision(4))
 
-const hundreds =10000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds =10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+//+++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6))
+// console.log(Math.sqrt(5))
+// console.log(Math.max(4,6,7,2))
+// console.log(Math.min(4,5,9,1))
+
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
 
