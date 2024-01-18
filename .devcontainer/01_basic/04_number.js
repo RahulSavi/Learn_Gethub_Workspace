2c7205f775abdd913ea5871149c974ae8a9e5986
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-01-18 05:52:59 +0000

    Number datatypes

---
+++ .devcontainer/01_basic/04_number.js
@@ -0,0 +1,17 @@
+const score =400
+console.log(score)
+
+const balance =new Number(100)
+
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const othernumer=235.23
+
+console.log(othernumer.toPrecision(4))
+
+const hundreds =10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
