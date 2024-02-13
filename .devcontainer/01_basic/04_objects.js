08d9ba785bb6a3e160d813424d4a90b09a3310dc
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-13 08:37:09 +0000

    siggleton obejct

---
+++ .devcontainer/01_basic/04_objects.js
@@ -0,0 +1,14 @@
+//const user_mgmt =new Object()//signlrton object
+
+// non singletone object.
+const user_mgmt ={}
+
+console.log(user_mgmt);
+
+user_mgmt.id ="1245abcd"
+
+user_mgmt.name="rahul"
+
+user_mgmt.inLoggedin=false
+
+//console.log(user_mgmt);
