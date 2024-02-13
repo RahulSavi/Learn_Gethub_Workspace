0f8075ed4bc05a4e99290c2e6a4b6d9cf48390e6
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-13 07:22:11 +0000

    learing object

---
+++ .devcontainer/03_objects.js
@@ -0,0 +1,10 @@
+const user_details={
+    name: "Rhaul",
+    "full name" : "Rahul Yadav",
+     age: 32,
+     location: "Pune",
+     isloggedin : false,
+     lastloginday :["Monday","Saturday"]
+}
+
+console.log(user_details.["full name"]);
