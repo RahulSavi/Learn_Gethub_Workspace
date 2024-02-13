0660771006e3a62ad40ee73e4a2406461d230fb8
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-13 10:14:13 +0000

    learning funtion

---
+++ .devcontainer/01_basic/03_Basisc/01_fucntion.js
@@ -0,0 +1,20 @@
+function print(){
+    console.log("Rhaul")
+    console.log("Ramesh")
+}
+
+print()
+
+function addnumber(num1,num2){
+
+    //console.log(num1+num2)
+
+    // let result =num1+ num2
+    // return result
+
+    return  num1+num2
+
+}
+const result=addnumber(3,4)
+
+console.log("Result:", result)
