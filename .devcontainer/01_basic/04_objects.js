2bc1b12f6c825212428b8a95f06aa99d6fd47067
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-13 09:08:23 +0000

    add two objects

--- .devcontainer/01_basic/04_objects.js
+++ .devcontainer/01_basic/04_objects.js
@@ -12,3 +12,26 @@ user_mgmt.name="rahul"
 user_mgmt.inLoggedin=false
 
 //console.log(user_mgmt);
+
+const regularuser={
+    email : "rahuly@logicmo.com",
+    fullname:{
+        userfullname:{
+            firstname: "logicMo",
+            lastname:"Systems"
+        }
+    }
+}
+// console.log(regularuser.fullname?.userfullname?.firstname)
+
+const objt1 = {1: "a", 2: "b"}
+const objt2 = {3: "c", 4: "d"}
+
+// const objt3 ={objt1,objt2}
+
+const objt3=Object.assign({},objt1,objt2)
+
+console.log(objt3)
+
+
+
