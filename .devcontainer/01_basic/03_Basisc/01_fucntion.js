56e8b3bc29921b8f7466b645abec4342845e3d83
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-14 11:25:52 +0000

    funtion & object

--- .devcontainer/01_basic/03_Basisc/01_fucntion.js
+++ .devcontainer/01_basic/03_Basisc/01_fucntion.js
@@ -18,3 +18,25 @@ function addnumber(num1,num2){
 const result=addnumber(3,4)
 
 console.log("Result:", result)
+
+//... rest oprator
+function calculatecartprice(...num1){
+    return num1
+}
+
+//console.log(calculatecartprice(200,400,250))
+
+const user ={
+    username:"Logicmo",
+    price:200
+}
+
+function handleObject(anyoneobject){
+    console.log(`user nam is ${anyoneobject.username} and price is ${anyoneobject.price}`)
+}
+handleObject(user)
+
+handleObject({
+    username:"Rahul",
+    price:299
+})
