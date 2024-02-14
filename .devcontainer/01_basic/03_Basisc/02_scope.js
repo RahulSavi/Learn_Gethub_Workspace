2bb746f6a67a1c5fcfe022c1c3d901943f868949
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-02-14 12:24:56 +0000

    hoisting learing

--- .devcontainer/01_basic/03_Basisc/02_scope.js
+++ .devcontainer/01_basic/03_Basisc/02_scope.js
@@ -6,35 +6,46 @@
 //console.log(a)
 //console.log(b)
 //BLOCK SCOPE
-if( true){
-    let a =20
-const b =20
-var c=30
-}
+// if( true){
+//     let a =20
+// const b =20
+// var c=30
+// }
 
-console.log(c)
+// console.log(c)
 
-function one(){
-    const username= "Rahul"
-    function two(){
-       const website ="Youtube"
-        console.log(username)
+// function one(){
+//     const username= "Rahul"
+//     function two(){
+//        const website ="Youtube"
+//         console.log(username)
         
-    }
+//     }
 
-    //console.log(website)
-    two()
-}
-one()
+//     //console.log(website)
+//     two()
+// }
+// one()
 
-if (true) {
-    const username ="Logicmo"
-    if (username=== "Logicmo") {
-        const website =" systems private Limited."
-        console.log(username + website)
+// if (true) {
+//     const username ="Logicmo"
+//     if (username=== "Logicmo") {
+//         const website =" systems private Limited."
+//         console.log(username + website)
         
-    }
-    //console.log(website)
-} else {
+//     }
+//     //console.log(website)
+// } else {
     
+// }
+console.log(addone(5))
+function addone(num){
+    return num +1
 }
+
+//addone()
+//addtwo(6)//hoisting
+const addtwo=function addtwo(num){
+    return num+1
+}
+console.log(addtwo(6))
