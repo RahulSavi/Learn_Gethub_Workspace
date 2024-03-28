32265f34a36d462da81cc70f78730f6f849b1b9f
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-03-28 12:42:09 +0000

    how to ingore the global pollution

--- .devcontainer/01_basic/03_Basisc/04_iife.js
+++ .devcontainer/01_basic/03_Basisc/04_iife.js
@@ -1,1 +1,25 @@
 //Immediately Invoked Funtion Expressions(IIFE)
+
+(function iife() {
+    console.log(`DB connection done..`);
+
+})();
+
+//()-dffiniation () -excution, using for ingnore the golbal scop Pollution
+
+(function iife2()
+//Named IIFE
+{
+    console.log(`DB connection done..`);
+
+})();
+
+(() => {
+    console.log(`DB connection done..3`);
+
+})();
+
+((Rahul) => {
+    console.log(`DB connection done ... ${Rahul}`);
+
+})('Database');
