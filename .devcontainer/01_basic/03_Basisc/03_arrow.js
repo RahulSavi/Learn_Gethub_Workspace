cffcd29deaff30522dba07532b3924eb6bd7fe12
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-03-28 08:42:06 +0000

    Learing arrow funtion

--- .devcontainer/01_basic/03_Basisc/03_arrow.js
+++ .devcontainer/01_basic/03_Basisc/03_arrow.js
@@ -3,6 +3,12 @@ const user={
     price:999,
     welcomeMessage: function(){
         console.log(`${this.username}, Welcome to LogicMo Systems private Limited`)
+        console.log(this);
     }
 
 }
+user.welcomeMessage()
+user.username ="Rahul Yadav"
+user.welcomeMessage()
+
+console.log(this);
