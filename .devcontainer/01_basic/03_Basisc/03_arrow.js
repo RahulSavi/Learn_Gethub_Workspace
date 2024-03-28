6879c09f4ae074ab58515f45a13c222756fe1de7
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-03-28 10:44:16 +0000

    learn more

--- .devcontainer/01_basic/03_Basisc/03_arrow.js
+++ .devcontainer/01_basic/03_Basisc/03_arrow.js
@@ -1,14 +1,30 @@
-const user={
-    username:"rahul",
-    price:999,
-    welcomeMessage: function(){
+const user = {
+    username: "rahul",
+    price: 999,
+    welcomeMessage: function () {
         console.log(`${this.username}, Welcome to LogicMo Systems private Limited`)
         console.log(this);
     }
 
 }
-user.welcomeMessage()
-user.username ="Rahul Yadav"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username ="Rahul Yadav"
+// user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
+// function learnjava(){
+//     let techcompname ="LogicMo Systems Private limited"
+//     console.log(this.username);
+// }
+// learnjava()
+
+// const mouse =function(){
+//     let mouse_make ="DELL"
+//     console.log(this)
+// }
+// mouse()
+
+const addtwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addtwo(5, 8));
