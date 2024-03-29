f644f47c2c464247e95a931c72ef14e9f09ef3a3
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-03-29 06:53:15 +0000

    lean if else Statement

---
+++ .devcontainer/01_basic/04_Control_flow/control.js
@@ -0,0 +1,11 @@
+const score=400
+const power= 'Atom bom'
+const power1='Mirchi Bom'
+if (score>300)
+{
+  
+    
+console.log(`user power :${power}`);
+}else{
+    console.log(`user power is  :${power1}`);
+}
