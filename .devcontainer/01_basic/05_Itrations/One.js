5c2db84af556628d30ee5c8c7a37c76d83f46839
Rahul Yadav <62686585+RahulSavi@users.noreply.github.com>
2024-03-30 05:20:11 +0000

    for loop learing

--- .devcontainer/01_basic/05_Itrations/One.js
+++ .devcontainer/01_basic/05_Itrations/One.js
@@ -18,16 +18,17 @@
     
 // }
 
-for (let i = 1; i <=10; i++) {
-    console.log(`Table of the :${i}`);
-    for (let j = 1; j <= 10; j++) {
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Table of the :${i}`);
+//     for (let j = 1; j <= 10; j++) {
+
+//         console.log(i + '*' + j + '=' + i*j);
         
-        console.log(i + '*' + j + '=' + i*j);
         
-        
-    }
+//     }
     
-}
+// }
+
 
 
 
